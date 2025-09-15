d3c972c5f89171d89af99df83b9937c057490287
Palak K <palakkongale@gmail.com>
2025-09-15 22:28:33 +0530

    done

---
+++ src/components/Cocktails.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { cocktailLists, mockTailLists } from '../../constants'
+
+const Cocktails = () => {
+  return (
+    <section id="cocktails" className='noisy'>
+     <img src='/images/cocktail-left-leaf.png' alf="left-leaf" id="c-left-leaf"/>
+     <img src='/images/cocktail-right-leaf.png' alf="right-leaf" id="c-right-leaf"/>
+     <div className='list'>
+        <div className='popular'>
+            <h2>Most Popular Cocktails:</h2>
+            <ul>{
+                cocktailLists.map(({name, country, detail, price})=>(
+                    <li key={name}>
+                       <div className='md:me-28'>
+                        <h3>{name}</h3>
+                        <p>{country} | {detail}</p>
+                       </div>
+                       <span>- {price}</span>
+                    </li>
+                ))
+                }</ul>
+        </div>
+        <div className='loved'>
+            <h2>Most Loved Mocktails:</h2>
+            <ul>{
+                mockTailLists.map(({name, country, detail, price})=>(
+                    <li key={name}>
+                       <div className='md:me-28'>
+                        <h3>{name}</h3>
+                        <p>{country} | {detail}</p>
+                       </div>
+                       <span>- {price}</span>
+                    </li>
+                ))
+                }</ul>
+        </div>
+     </div>
+    </section>
+  )
+}
+
+export default Cocktails
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,12 @@ import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
 import { SplitText } from 'gsap/all'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import React from 'react'
+import React, { use,useRef } from 'react'
+import { useMediaQuery } from 'react-responsive'
 
 const Hero = () => {
+    const videoRef=useRef()
+    const isMobile=useMediaQuery({maxWidth:768})
     useGSAP(()=>{
        const heroSplit= new SplitText('.title',{type:'chars,words'})
        const paragraphSplit= new SplitText('.subtitle',{type:'lines'})
@@ -36,6 +39,26 @@ gsap.timeline({
 .to('.left-leaf',{y:-200},0)
 .to('.right-leaf',{y:200},0)
 
+const startValue=isMobile ? 'top 50%' : 'center 60%';
+const endValue=isMobile ? '120% top' :'bottom top';
+
+const tl=gsap.timeline({
+    scrollTrigger:{
+        trigger:'video',
+        start:startValue,
+        end:endValue,
+        scrub:true,
+        pin:true,
+
+    }
+})
+
+videoRef.current.onloadedmetadata = () =>{
+    tl.to(videoRef.current,{
+        currentTime:videoRef.current.duration,
+    })
+}
+
     },[])
   return (
    <>
@@ -63,6 +86,14 @@ creative flair, and timeless recipes
         </div>
     </div>
    </section>
+   <div className='video absolute inset-0'>
+    <video src='/videos/output.mp4'
+    ref={videoRef}
+    muted
+    playsInline
+    preload='auto'
+    />
+   </div>
    </>
   )
 }
