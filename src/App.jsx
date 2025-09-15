c69d238d10aa1602e5b3093dfcc23aa3e032ec1b
Palak K <palakkongale@gmail.com>
2025-09-15 17:31:36 +0530

    utilities, layers and components added in indexs.css and they are working fine

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ gsap.registerPlugin(ScrollTrigger, SplitText)
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-3xl text-indigo-300'>Hello GSAP</h1>
+    <div className='flex-center h-[100vh]'>
+      <h1 className='text-3xl text-indigo-300 '>Hello GSAP</h1>
     </div>
   )
 }
